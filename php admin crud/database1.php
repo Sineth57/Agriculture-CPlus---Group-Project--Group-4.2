78fc9287b52814ede81c393ccdfba6c142d29b44
Akash Induwara <akashinduwara252@gmail.com>
2023-11-21 15:52:32 +0530

    Update UI of list page

--- php admin crud/database1.php
+++ php admin crud/database1.php
@@ -46,6 +46,182 @@ if (isset($_POST['submit'])) {
     <link rel="shortcut icon" type="x-icon" href="logo.png">
     <link rel="stylesheet" href="css/style.css">
     <title>CPlus Products</title>
+
+    <style>
+        body {
+            font-family: 'Arial', sans-serif;
+            margin: 0;
+            padding: 0;
+            background-color: #f4f4f4;
+        }
+
+        .search-container {
+            text-align: center;
+            padding: 20px;
+        }
+
+        .search-bar {
+            width: 70%;
+            max-width: 800px;
+            padding: 10px;
+            border: 2px solid #ccc;
+            border-radius: 5px;
+            margin-right: 10px;
+            box-sizing: border-box;
+        }
+
+        .search-button {
+            padding: 10px 20px;
+            background-color: #4CAF50;
+            color: white;
+            border: none;
+            border-radius: 5px;
+            cursor: pointer;
+            box-sizing: border-box;
+            transition: background-color 0.3s ease;
+        }
+
+        .search-button:hover {
+            background-color: #45a049;
+        }
+
+        .product-card {
+            display: flex;
+            max-width: 1250px;
+            margin: 20px auto;
+            padding: 10px;
+            background-color: #f1eeee;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            min-height: 300px;
+            border: 2px solid #0c0c0c;
+            border-radius: 10px;
+            overflow: hidden;
+            transition: transform 0.3s ease;
+        }
+        
+        .product-card:hover {
+            transform: scale(1.05);
+        }
+        
+        .product-image {
+            width: 40%;
+            padding-right: 10px;
+        }
+        
+        .product-image img {
+            width: 100%;
+            height: auto;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            flex-grow: 1;
+        }
+        
+        .product-details {
+            width: 60%;
+            display: flex;
+            flex-direction: column;
+            justify-content: space-between;
+            padding: 10px;
+        }
+        
+        .product-details h3 {
+            margin: 0;
+            font-size: 18px;
+        }
+        
+        .product-details p {
+            margin-bottom: 15px;
+            font-size: 14px;
+        }
+        
+        .product-price {
+            color: #4CAF50;
+            font-weight: bold;
+        }
+        
+        .contact-button {
+            display: inline-block;
+            padding: 10px;
+            background-color: #4CAF50;
+            color: white;
+            text-decoration: none;
+            border-radius: 5px;
+            transition: background-color 0.3s ease;
+        }
+        
+        .contact-button:hover {
+            background-color: #45a049;
+        }
+
+        .flex-container {
+            display: flex;
+            flex-wrap: wrap;
+            justify-content: space-between;
+        }
+
+        .flex-box {
+            display: flex;
+            width: 48%;
+            max-width: 1250px;
+            margin: 20px auto;
+            margin-bottom: 20px;
+            padding: 10px;
+            background-color: #f1eeee;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            box-sizing: border-box;
+            min-height: 300px;
+            border: 2px solid #0c0c0c;
+            border-radius: 10px;
+            overflow: hidden;
+            transition: transform 0.3s ease;
+        }
+
+        .flex-box:hover {
+            transform: scale(1.05);
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.5);
+        }
+        
+        .left {
+            width: 40%;
+            padding-right: 10px;
+        }
+        
+        .product-image img {
+            width: 100%;
+            height: auto;
+            border-radius: 8px;
+            box-shadow: 0 0 10px rgba(0, 0, 0, 0.1);
+            flex-grow: 1;
+        }
+        
+        
+        .right {
+            width: 60%;
+            flex-direction: column;
+            justify-content: space-between;
+        }
+        
+        .product-id, .product-name, .product-description, .phone-number, .product-price {
+            padding: 10px;
+            font-size: 18px;
+            margin-bottom: 25px;
+        }
+
+        @media screen and (max-width: 600px) {
+            .search-bar,
+            .search-button {
+                width: 100%;
+                margin-right: 0;
+            }
+        }
+
+        @media screen and (max-width: 768px) {
+            .flex-box {
+                width: 100%;
+            }
+        }
+    </style>
+
 </head>
 
 <body style="background-image: url('B1.jpg');">
@@ -95,101 +271,81 @@ if (isset($_POST['submit'])) {
 
     <br><br><br>
     <div class="product-display">
-        <form action="" method="POST">
-            <input class="searchbar" type="text" name="search" placeholder="Search products...">
-            <input class="searchbutton" type="submit" name="submit" value="Search">
-        </form>
+        <div class="search-container">
+            <form action="" method="POST">
+                <input class="search-bar" type="text" name="search" placeholder="Search products...">
+                <input class="search-button" type="submit" name="submit" value="Search">
+            </form>
+        </div>
 
         <br><br>
-        <table class="product-display-table">
-            <thead>
-                <tr style="background-color: green; color:white">
-                    <th>Item ID</th>
-                    <th>product image</th>
-                    <th>product Name</th>
-                    <th>product price</th>
-                    <th>product description</th>
-                    <th>Contact</th>
-                </tr>
-            </thead>
 
-            <?php
-            if (isset($_POST['submit'])) {
-                if ($result && mysqli_num_rows($result) > 0) {
-                    while ($row = mysqli_fetch_assoc($result)) {
-                        echo '<tr>';
-                        echo '<td>' . $row['pid'] . '</td>';
-                        echo "<td><img src='uploaded_img/" . $row['image'] . "' height='100' alt=''></td>";
-                        echo '<td>' . $row['name'] . '</td>';
-                        echo '<td>' . $row['price'] . '</td>';
-                        echo '<td>' . $row['description'] . '</td>';
-
-                        echo '<td>';
-                        echo '<a href="userDetails.php?pid=' . $row['pid'] . '" class="btn">';
-                        echo '<i class="fas fa-edit"></i> Contact';
-                        echo '</a>';
-
-                        echo '<form method="POST" action="cart.php">';
-                        echo '<input type="hidden" name="product_id" value="' . $row['pid'] . '">';
-                        echo '<input type="submit" name="add_to_cart" value="Add to Cart" class="btn">';
-                        echo '</form>';
-                        echo '</td>';
-
-                        echo '</tr>';
-                    }
-                } else {
-                    echo 'No results found.';
+        <?php
+        if (isset($_POST['submit'])) {
+            if (mysqli_num_rows($result) > 0) {
+                while ($row = mysqli_fetch_assoc($result)) {
+                    echo '<div class="product-card">';
+                    echo '<div class="product-image">';
+                    echo "<img src='uploaded_img/" . $row['image'] . "' alt='Product Image'>";
+                    echo '</div>';
+                    echo '<div class="product-details">';
+                    echo '<h3>Product Name: ' . $row['name'] . '</h3>';
+                    echo '<p><strong>Item ID:</strong> ' . $row['pid'] . '</p>';
+                    echo '<p><strong>Price:</strong> Rs. ' . $row['price'] . '/-</p>';
+                    echo '<p><strong>Description:</strong> ' . $row['description'] . '</p>';
+                    echo '<a href="userDetails.php?pid=' . $row['pid'] . '" class="contact-button">';
+                    echo '<i class="fas fa-edit"></i> Contact';
+                    echo '</a>';
+                    echo '</div>';
+                    echo '</div>';
                 }
+            } else {
+                echo 'No results found.';
             }
-            ?>
-        </table>
+        }
+        ?>
+        
     </div>
 
     <br><br>
 
-    <?php
-    $select = mysqli_query($conn, 'SELECT * FROM products');
-    ?>
+    <?php $select = mysqli_query($conn, 'SELECT * FROM products'); ?>
     <div class="product-display">
         <h2 class="seemore">See newly listed products</h2>
+
         <br>
-        <table class="product-display-table">
-            <thead>
-                <tr style="background-color: green; color:white">
-                    <th>Item ID</th>
-                    <th>product image</th>
-                    <th>product name</th>
-                    <th>product price</th>
-                    <th>product description</th>
-                    <th>Contact</th>
-                </tr>
-            </thead>
 
-            <?php
-            while ($row = mysqli_fetch_assoc($select)) {
-            ?>
-                <tr>
-                    <td><?php echo $row['pid']; ?></td>
-                    <td><img src="uploaded_img/<?php echo $row['image']; ?>" height="100" alt=""></td>
-                    <td><?php echo $row['name']; ?></td>
-                    <td>Rs.<?php echo $row['price']; ?>/-</td>
-                    <td><?php echo $row['description']; ?></td>
-
-                    <td>
-                        <a href="userDetails.php?pid=<?php echo $row['pid']; ?>" class="btn">
+        <div class="flex-container">
+        
+            <?php while ($row = mysqli_fetch_assoc($select)) { ?>
+            
+                <div class="flex-box">
+                    <div class="left">
+                        <div class="product-image">
+                            <img src="uploaded_img/<?php echo $row['image']; ?>" alt="Product Image">
+                        </div>
+                        
+                    </div>
+                    <div class="right">
+                        <div class="product-name">
+                            <p>Product Name: <?php echo $row['name']; ?></p>
+                        </div>
+                        <div class="product-description">
+                            <p>Description: <?php echo $row['description']; ?></p>
+                        </div>
+                        <div class="product-price">
+                            <p>Price: Rs. <?php echo $row['price']; ?>/-</p>
+                        </div>
+                        <div class="button"><a href="userDetails.php?id=<?php echo $row['id']; ?>" class="btn">
                             <i class="fas fa-edit"></i> Contact
-                        </a>
-
+                        </a></div>
                         <form method="POST" action="cart.php">
                             <input type="hidden" name="product_id" value="<?php echo $row['pid']; ?>">
                             <input type="submit" name="add_to_cart" value="Add to Cart" class="btn">
                         </form>
-                    </td>
-                </tr>
-            <?php
-            }
-            ?>
-        </table>
+                    </div>
+                </div>
+            <?php } ?>
     </div>
 </body>
 
